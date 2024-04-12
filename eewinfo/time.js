66386f3a3ccac8f50989f85985172238e544f1d2
Nooooooo <113202604+Nooooooo-0328@users.noreply.github.com>
2024-04-13 02:59:27 +0900

    Delete eewinfo directory

--- eewinfo/time.js
+++
@@ -1,8 +0,0 @@
-function updateCurrentTime() {
-  const currentTimeElement = document.getElementById('current-time');
-  const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-  const currentTime = new Date().toLocaleString('ja-JP', options);
-  currentTimeElement.textContent = currentTime;
-}
-
-setInterval(updateCurrentTime, 1000);
